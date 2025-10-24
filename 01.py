bf4892d061cde887064582147fd4a56b835c9e12
Rizvi Mohd Tusi <sayedtusi@gmail.com>
2025-10-24 22:58:00 +0530

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,22 @@
+#Task 1
+'''
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+
+print("The Addition of the numbers are:",a+b)
+
+print("The Subtraction of the numbers are:",a-b)
+
+print("The Multiplication of the numbers are:",a*b)
+
+print("The Division of the numbers are:",a/b)
+'''
+#---------------------------------------------------------------------------------------
+
+#Task 2
+
+a=input("enter your first name:")
+b=input("enter your last name:" )
+
+print(f"Hello,\n {a +" "+ b} \n welcome to python programming")
+
